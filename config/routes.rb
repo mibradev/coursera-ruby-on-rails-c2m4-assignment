dac30d3fa4d5f3102485f49a908268dfd933fd67
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2019-02-07 09:59:47 +0200

    Create Sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  delete "/logout" => "sessions#destroy"
+  get "/login" => "sessions#new"
+  resources :sessions, only: [:new, :create, :destroy]
+
   root "todo_lists#index"
 
   resources :todo_lists do
